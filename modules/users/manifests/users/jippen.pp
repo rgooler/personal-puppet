f24a9ec7ef281dc01eab2339729e295a6312d7f4
root <root@nas.goolertech.com>
2011-12-14 22:19:31 -0800

    Better user management

--- modules/users/manifests/users/jippen.pp
+++ modules/users/manifests/users/jippen.pp
@@ -1,24 +1,12 @@
 class users::users::jippen{
-  include 'users::secret'
+  include 'users::util'
 
   #My non-root account I use everywhere
-  user {'jippen':
-    ensure     => present,
-    gid        => 'wheel',
-    home       => '/home/jippen',
-    managehome => true,
-    shell      => '/bin/bash',
-    uid        => 1000,
-    password   => $users::secret::jippen,
-  }
-
-  file {'/home/jippen':
-    ensure  => directory,
-    mode    => 0700,
-    owner   => 'jippen',
-    group   => 'wheel',
-    require => User['jippen'],
-    recurse => false,
+  user_acct {'jippen':
+    fullname => "Jippen Faddoul",
+    group    => 'wheel',
+    uid      => 1000,
+    pass     => $users::secret::jippen,
   }
 
 }
---
+++ modules/users/manifests/util.pp
@@ -0,0 +1,26 @@
+include 'users::secret'
+
+define user_acct($fullname, $group, $uid, $pass='!!',$groups=[]){
+  user { $name:
+    ensure     => present,
+    gid        => $group,
+    home       => "/home/${name}",
+    managehome => true,
+    shell      => '/bin/bash',
+    uid        => $uid,
+    password   => $pass,
+  }
+
+  file { "/home/$name":
+    ensure  => directory,
+    mode    => 0700,
+    owner   => $name,
+    group   => $group,
+    require => User[$name],
+    recurse => false,
+  }
+}
+
+
+class users::util{}
+
