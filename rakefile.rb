d6dd0e311f3d027d54ee0d1cb0362ffe303b622a
root <root@nas.goolertech.com>
2011-12-15 23:27:20 -0800

    Adding Rake file for ensuring that puppet has valid syntax

---
+++ rakefile.rb
@@ -0,0 +1,19 @@
+require 'rake'
+require 'rake/tasklib'
+
+desc "Check puppet syntax"
+task :default do
+  puppet = '/usr/bin/puppet'
+  params = '--parseonly --ignoreimport'
+
+  [ 'manifests/*.pp', 'modules/*/manifests/*.pp' ].each do |path|
+    Dir.glob( path ).each do |file|
+      sh "#{ puppet } #{ params } #{ file }" do |ok, res|
+        unless ok
+          abort
+        end
+      end
+    end
+  end
+end
+
