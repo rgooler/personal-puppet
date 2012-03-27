b37681f97047963adeb5cec01e663305467f2b2d
root <root@localhost.localdomain>
2012-03-27 01:29:56 -0700

    Adding default rtorrentrc

--- modules/rtorrent/manifests/init.pp
+++ modules/rtorrent/manifests/init.pp
@@ -2,4 +2,12 @@ class rtorrent{
 
   include 'rtorrent::install'
   include 'rtorrent::user'
+
+  file{'/home/rtorrent/.rtorrentrc':
+    ensure  => file,
+    mode    => '0600',
+    owner   => 'rtorrent',
+    group   => 'rtorrent',
+    content => template('rtorrent/rtorrentrc.erb'),
+  }
 }
