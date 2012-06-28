a7b6a665b033f788ebd10cf8d284abf0e4970936
root <root@localhost.localdomain>
2012-06-27 20:51:51 -0700

    Fixing ejabberd stuff

--- modules/ejabberd/manifests/configuration.pp
+++ modules/ejabberd/manifests/configuration.pp
@@ -5,13 +5,19 @@ class ejabberd::configuration {
   }
 
   file { '/etc/ejabberd/ejabberd.cfg':
-    ensure => file,
-    source => 'puppet:///modules/ejabberd/ejabberd.cfg',
+    ensure  => file,
+    owner   => 'ejabberd',
+    group   => 'ejabberd',
+    mode    => 0640,
+    source  => 'puppet:///modules/ejabberd/ejabberd.cfg',
     require => File['/etc/ejabberd'],
   }
 
   file { '/etc/ejabberd/inetrc':
     ensure => file,
+    owner   => 'ejabberd',
+    group   => 'ejabberd',
+    mode    => 0640,
     source => 'puppet:///modules/ejabberd/inetrc',
     require => File['/etc/ejabberd'],
   }
