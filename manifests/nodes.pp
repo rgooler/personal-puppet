30c995c9b7dad3dd909e9fa0ac740bd6bc2e8686
Ryan Gooler <cheetahmorph@gmail.com>
2012-08-27 00:42:02 -0700

    Add personal user stuff to any system I run puppet apply on

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,8 @@
 node 'openmediavault.goolertech.com' inherits nas{
   include 'ejabberd'
 }
+
+node default {
+  include 'users::users::jippen'
+  include 'puppet::scripts'
+}
--- modules/users/manifests/users/jippen.pp
+++ modules/users/manifests/users/jippen.pp
@@ -6,15 +6,49 @@ class users::users::jippen{
     fullname => "Jippen Faddoul",
     group    => 'wheel',
     uid      => 1000,
-    pass     => $users::secret::jippen,
   }
 
   file {'/home/jippen/.ssh/':
-    owner => 'jippen',
-    group => 'wheel',
-    mode => 0600,
+    owner   => 'jippen',
+    group   => 'wheel',
+    mode    => 0600,
     recurse => true,
     require => User_acct['jippen'],
-    source => 'puppet:///modules/users/jippen/.ssh',
+    source  => 'puppet:///modules/users/jippen/.ssh',
   }
+
+  file { '/home/jippen/.bashrc':
+    owner   => 'jippen',
+    group   => 'wheel',
+    mode    => 0600,
+    require => User_acct['jippen'],
+    source  => 'puppet:///modules/users/jippen/.bashrc'
+  }
+
+  file { '/home/jippen/.inputrc':
+    owner   => 'jippen',
+    group   => 'wheel',
+    mode    => 0600,
+    require => User_acct['jippen'],
+    source  => 'puppet:///modules/users/jippen/.inputrc'
+  }
+
+  file { '/home/jippen/.vim':
+    owner   => 'jippen',
+    group   => 'wheel',
+    ensure  => directory,
+    mode    => 0600,
+    require => User_acct['jippen'],
+    recurse => true,
+    source  => 'puppet:///modules/users/jippen/.vim'
+  }
+
+  file { '/home/jippen/.vimrc':
+    ensure => link,
+    target => '/home/jippen/.vim/.vimrc',
+    owner   => 'jippen',
+    group   => 'wheel',
+    require => [ User_acct['jippen'], File['/home/jippen/.vim'] ],
+  }
+
 }
