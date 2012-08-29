e5fccef3f6ad41313fcb6d556fd03cc475d76ad5
Ryan Gooler <cheetahmorph@gmail.com>
2012-08-29 01:49:17 -0700

    Fixing download definition

--- modules/common/manifests/defines/download.pp
+++ modules/common/manifests/defines/download.pp
@@ -2,7 +2,7 @@ class common::defines::download{}
 
 define download($mirror,$to='/usr/local/src',$flags=''){
 
-    exec{"wget ${flags} ${mirror}/${name}":
+    exec{"/usr/bin/wget ${flags} ${mirror}/${name}":
         cwd     => $to,
         creates => "${to}/${name}"
     }
