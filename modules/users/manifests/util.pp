b8717b0b2161c280547ac252be9c76aab645635f
Ryan Gooler <cheetahmorph@gmail.com>
2012-12-15 19:55:51 -0800

    Fix user module

--- modules/users/manifests/util.pp
+++ modules/users/manifests/util.pp
@@ -7,21 +7,21 @@ define user_acct($fullname, $uid, $groups=[]){
   user { $name:
     ensure     => present,
     gid        => $name,
-    home       => "/home/${name}",
-    managehome => true,
+    #home       => "/home/${name}",
+    #managehome => true,
     shell      => '/bin/bash',
     uid        => $uid,
     require    => Group[$name],
   }
 
-  file { "/home/${name}":
-    ensure  => directory,
-    mode    => 0700,
-    owner   => $name,
-    group   => $group,
-    require => User[$name],
-    recurse => false,
-  }
+  #file { "/home/${name}":
+  #  ensure  => directory,
+  #  mode    => 0700,
+  #  owner   => $name,
+  #  group   => $group,
+  #  require => User[$name],
+  #  recurse => false,
+  #}
 }
 
 
