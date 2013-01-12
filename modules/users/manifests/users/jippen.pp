efd48cb5354a168fb72c4bd4f6cd833cbcf243f5
Ryan Gooler <cheetahmorph@gmail.com>
2013-01-11 21:52:00 -0800

    Update modules/users/manifests/users/jippen.pp

--- modules/users/manifests/users/jippen.pp
+++ modules/users/manifests/users/jippen.pp
@@ -3,51 +3,17 @@ class users::users::jippen{
 
   #My non-root account I use everywhere
   user_acct {'jippen':
-    fullname => "Jippen Faddoul",
-    uid      => 1000,
-  }
-
-  file {'/home/jippen/.ssh/':
-    owner   => 'jippen',
-    group   => 'jippen',
-    mode    => 0600,
-    recurse => true,
-    require => User_acct['jippen'],
-    source  => 'puppet:///modules/users/jippen/.ssh',
-  }
-
-  file { '/home/jippen/.bashrc':
-    owner   => 'jippen',
-    group   => 'jippen',
-    mode    => 0600,
-    require => User_acct['jippen'],
-    source  => 'puppet:///modules/users/jippen/.bashrc'
-  }
-
-  file { '/home/jippen/.inputrc':
-    owner   => 'jippen',
-    group   => 'jippen',
-    mode    => 0600,
-    require => User_acct['jippen'],
-    source  => 'puppet:///modules/users/jippen/.inputrc'
-  }
-
-  file { '/home/jippen/.vim':
-    owner   => 'jippen',
-    group   => 'jippen',
-    ensure  => directory,
-    mode    => 0600,
-    require => User_acct['jippen'],
-    recurse => true,
-    source  => 'puppet:///modules/users/jippen/.vim'
+    fullname    => "Jippen Faddoul",
+    uid         => 1000,
+    recursehome => true,
   }
 
   file { '/home/jippen/.vimrc':
-    ensure => link,
-    target => '/home/jippen/.vim/.vimrc',
+    ensure  => link,
+    target  => '/home/jippen/.vim/.vimrc',
     owner   => 'jippen',
     group   => 'jippen',
-    require => [ User_acct['jippen'], File['/home/jippen/.vim'] ],
+    require => User_acct['jippen'],
   }
 
 }
