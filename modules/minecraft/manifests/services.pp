d63c36c64f05de219912a7b1b73b1e62341e0597
Claem <Claem@nfs.goolerhome.com>
2013-06-09 15:59:45 -0700

    Added minecraft executable

--- modules/minecraft/manifests/services.pp
+++ modules/minecraft/manifests/services.pp
@@ -7,4 +7,15 @@ class minecraft::services {
     mode   => '0755',
     source => "puppet:///modules/minecraft/etc/init.d/minecraft",  
   }
+
+  file { '/home/minecraft/minecraft/minecraft.sh'
+    ensure  => file,
+    owner   => 'minecraft',
+    group   => 'minecraft',
+    mode    => '0711',
+    source  => "puppet:///modules/minecraft/home/minecraft/minecraft/minecraft.sh",
+  }
 }
+
+
+
