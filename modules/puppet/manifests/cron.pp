2c198e641c0d4ea524593af7c67c611cd4370c0b
Ryan Gooler <cheetahmorph@gmail.com>
2014-06-24 11:31:12 -0700

    Quiet puppet logs

--- modules/puppet/manifests/cron.pp
+++ modules/puppet/manifests/cron.pp
@@ -2,7 +2,7 @@ class puppet::cron {
 
   better_cron{ 'puppet':
     cron_minute => fqdn_rand(60),
-    path   => '/usr/local/sbin/update_puppet.sh -q',
+    path        => '/usr/local/sbin/update_puppet.sh > /var/log/puppet/lastrun || cat /var/log/puppet/lastrun;cat /var/log/puppet/lastrun >> /var/log/puppet/puppet.log',
   }
 
 }
