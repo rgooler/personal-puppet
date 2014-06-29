7ea9adeafda19ce229411d379d3990d1eded1179
Ryan Gooler <cheetahmorph@gmail.com>
2014-06-29 02:26:04 -0700

    Fix squid dirs

--- modules/squid/manifests/configuration.pp
+++ modules/squid/manifests/configuration.pp
@@ -1,14 +1,14 @@
 class squid::configuration { 
 
-  file { '/etc/squid/squid3.conf':
+  file { '/etc/squid3/squid.conf':
     ensure => file,
     mode   => 0644,
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/squid/etc/squid/squid3.conf',
+    source => 'puppet:///modules/squid3/etc/squid/squid.conf',
   }
 
-  file { '/etc/squid/squid.d':
+  file { '/etc/squid3/squid.d':
     ensure => directory,
  }
 
