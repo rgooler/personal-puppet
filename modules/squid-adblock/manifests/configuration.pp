0285498d945a30095f259e938da22cd9734a9b8d
Ryan Gooler <cheetahmorph@gmail.com>
2014-06-29 01:58:30 -0700

    Default squid config

--- modules/squid-adblock/manifests/configuration.pp
+++ modules/squid-adblock/manifests/configuration.pp
@@ -1,3 +1,11 @@
 class squid-adblock::configuration { 
 
+  file { '/etc/squid/squid3.conf':
+    ensure => file
+    mode   => 0644,
+    user   => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/squid-adblock/etc/squid/squid3.conf',
+  }
+
 }
