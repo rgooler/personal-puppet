f38333e0bb04da4a2f90762d3d8b0867239ddadb
Jippen Faddoul <jippen@nfs.thisstuffismine.com>
2014-06-29 01:54:03 -0700

    Improve squid

--- modules/squid-adblock/manifests/install.pp
+++ modules/squid-adblock/manifests/install.pp
@@ -1,3 +1,7 @@
 class squid-adblock::install { 
 
+  package { 'squid':
+    ensure => latest,
+  }
+
 }
--- modules/squid-adblock/manifests/services.pp
+++ modules/squid-adblock/manifests/services.pp
@@ -1,3 +1,9 @@
 class squid-adblock::services { 
 
+  service { 'squid3':
+    ensure    => running,
+    enable    => true,
+    subscribe => Class['squid-adblock::configuration'],
+  }
+
 }
